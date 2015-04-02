81fa9df9e1b7cc66e11c870a0c695d15a5e7bf25
Adam Butler <adam@lab.io>
2015-04-02 12:35:18 +0100

    Testing returning events

--- app.rb
+++ app.rb
@@ -3,17 +3,29 @@ require 'httparty'
 
 class Meetup
   include HTTParty
+
   base_uri 'api.meetup.com'
 
   def initialize(api_key)
-    @options = { query: { key: api_key } }
+    @options = {
+      query: {
+        key: api_key
+      }
+    }
   end
 
   def events(options = {})
     options = @options.dup.deep_merge({query: options})
-    self.class.get("/2/events", options)
+    response = self.class.get("/2/events", options)
+    case response.code
+    when 200
+      return response["results"]
+    end
   end
 end
 
 meetup = Meetup.new("")
-puts meetup.events({ group_urlname: "bristoljs" }).body
+
+meetup.events({ group_urlname: "bristoljs" }).each do |event|
+  puts event["name"]
+end
