8934be499ee0bb12ba3e9e1d0960c789f7b36425
Adam Butler <adam@lab.io>
2015-04-04 11:05:16 +0100

    Wire up .env file

--- spec/meetup_spec.rb
+++ spec/meetup_spec.rb
@@ -3,12 +3,13 @@ require 'meetup'
 RSpec.describe Meetup do
   before(:all) do
     Meetup.configure do |config|
-      config.api_key = ""
+      config.api_key = ENV["MEETUP_API_KEY"]
     end
   end
 
   it "returns events" do
-    events = Meetup.events({ group_urlname: "bristoljs" })
+    events = Meetup.events({ group_urlname: ENV["MEETUP_GROUP_URL_NAME"] })
+    expect(events).to be_a(Array)
   end
 
 end
